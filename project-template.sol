8bb56e919e24d3faef30eba14acb7de472b10652
sbelka-1703 <sbelka93@gmail.com>
2021-10-04 12:55:38 -0700

    Star here

--- project-template.sol
+++ project-template.sol
@@ -1,7 +1,10 @@
+//Start here 
+
 pragma solidity 0.7.5;
 pragma abicoder v2;
 
 contract Wallet {
+   
     address[] public owners;
     uint limit;
     
@@ -19,22 +22,9 @@ contract Wallet {
     
     //Should only allow people in the owners list to continue the execution.
     modifier onlyOwners(){
-        bool owner = false;
-
-        for(uint i = 0; i <= owners; i++){
-            if (owners[i] == msg.sender){
-                owner = true; 
-            }
-            
-
-        }
-        require(owner == true);
-        _;
     }
     //Should initialize the owners list and the limit 
     constructor(address[] memory _owners, uint _limit) {
-        owners = _owners;
-        limit = _limit;
     }
     
     //Empty function
@@ -42,7 +32,6 @@ contract Wallet {
     
     //Create an instance of the Transfer struct and add it to the transferRequests array
     function createTransfer(uint _amount, address payable _receiver) public onlyOwners {
-        transferRequests.push(Transfer(_ammount,_receiver, 0, false, transferRequests.lenght));
         
     }
     
@@ -53,28 +42,7 @@ contract Wallet {
     //An owner should not be able to vote twice.
     //An owner should not be able to vote on a tranfer request that has already been sent.
     function approve(uint _id) public onlyOwners {
-        
-        
-        for(uint i = 0; i <= owners.length; i++ ){
-        require(approvals[msg.sender][_id] == false);
-        require(tranferRequests[_id].hasBeenSent == false);
-        transferRequests[_id].approvals++;
-        approvals[msg.sender][_id] = true;
-        if(Tranfer.approvals >= limit){
-            transferRequests[_id].send(transferRequests[_id].ammount);
-        }
-        }
-
-
-
     }
     
     //Should return all transfer requests
     function getTransferRequests() public view returns (Transfer[] memory){
-        
-            return transferRequests;
-    
-    }
-    
-    
-}
