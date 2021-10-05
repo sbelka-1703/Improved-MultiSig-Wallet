c4769ad4e5afa8db0ab8a11753d8ca07c86986fa
sbelka-1703 <sbelka93@gmail.com>
2021-10-05 07:48:36 -0700

    Attempt-2 (done without checking)

--- project-template.sol
+++ project-template.sol
@@ -21,9 +21,21 @@ contract Wallet {
     
     //Should only allow people in the owners list to continue the execution.
     modifier onlyOwners(){
+        bool owners = false;
+
+        for(uint i = 0; i <= owners.length; i++){
+            if (owners[i] == msg.sender){
+                owners = true;
+            }
+        }
+
+        require(owners == true);
+        _;
     }
     //Should initialize the owners list and the limit 
     constructor(address[] memory _owners, uint _limit) {
+        owners = _owners;
+        limit = _limit;
     }
     
     //Empty function
@@ -31,6 +43,8 @@ contract Wallet {
     
     //Create an instance of the Transfer struct and add it to the transferRequests array
     function createTransfer(uint _amount, address payable _receiver) public onlyOwners {
+
+        transferRequests.push(Transfer(_amount, _receiver, 0, false, transferRequest.lenght));
         
     }
     
@@ -41,7 +55,22 @@ contract Wallet {
     //An owner should not be able to vote twice.
     //An owner should not be able to vote on a tranfer request that has already been sent.
     function approve(uint _id) public onlyOwners {
+        require(approvals[msg.sender][_id] == false);
+        require(transferRequests[_id].hasBeenSent == false);
+ 
+        approvals[msg.sender][_id] = true;
+        transferRequests[_id].approvals++;
+        transferRequests[_id].hasBeenSent = true;
+
+        if(limit <= transferRequests[_id].approvals){
+            transferRequests[_id].reciver.transfer(transferRequests[_id].amount);
+        }
+
+
     }
     
     //Should return all transfer requests
     function getTransferRequests() public view returns (Transfer[] memory){
+        transferRequests;
+
+}
