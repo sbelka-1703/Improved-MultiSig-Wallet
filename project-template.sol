11ac768b351e28b6ca9d26e39f3c6a1d4eeb92a7
sbelka-1703 <sbelka93@gmail.com>
2021-10-08 08:13:59 -0700

    Minor update

--- project-template.sol
+++ project-template.sol
@@ -2,7 +2,7 @@ pragma solidity 0.7.5;
 pragma abicoder v2;
 
 
-//["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c"]
+
 
 contract Wallet {
     
